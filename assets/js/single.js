2fb589e94e9a760be73ac030a2a4ec2bfd5eccec
Eliadad <eliasashtonsaunders@gmail.com>
2021-04-15 17:21:16 -0700

    added link between html pages using split methods on strings

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,16 @@
 var issuesContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    var queryString = document.location.search;
+    
+    var repoName = queryString.split("=")[1];
+
+    getRepoIssues(repoName);
+
+    repoNameEl.textContent = repoName;
+}
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
@@ -62,4 +72,5 @@ var displayWarning = function(repo) {
 
     limitWarningEl.appendChild(linkEl);
 }
-getRepoIssues("facebook/react");
+
+getRepoName();
